51a32b67d7a07a442b0c558fcd3f7fa6ba895395
Tanmoy <Tanmoy@Tanmoy-PC>
2019-05-01 12:06:51 +0530

    Added a function to read a Json File and convert it into String format

--- RestAssuredWithTwitterAPI/src/main/java/reusableMethods/ReusableMethods.java
+++ RestAssuredWithTwitterAPI/src/main/java/reusableMethods/ReusableMethods.java
@@ -1,5 +1,9 @@
 package reusableMethods;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
 import io.restassured.path.json.JsonPath;
 import io.restassured.path.xml.XmlPath;
 import io.restassured.response.Response;
@@ -14,6 +18,11 @@ public class ReusableMethods {
 	public static XmlPath rawToXML(Response response)
 	{
 		return new XmlPath(response.asString());
+	}
+	
+	public static String generateStringFromJson(String pathOfJsonFile) throws IOException
+	{
+		return new String(Files.readAllBytes(Paths.get(pathOfJsonFile)));
 	}
 
 }
